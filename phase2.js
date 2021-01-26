3157985701deb738db16aee9d83f14b7a7ff4670
coralgad1 <coralgad98@gmail.com>
2021-01-26 09:13:12 +0200

    phase 2

--- phase2.js
+++ phase2.js
@@ -1,4 +1,13 @@
+let userString = localStorage.getItem('User');
+let UserObj = {};
+if (userString){
+    UserObj = JSON.parse(userString);
+}
+else{
+    alert("YOU DONT BELONG HERE!!!");
+    // location.href = 'doom.com';
 
+}
 window.onload = function(){
     document.querySelector("#nextBtn").addEventListener("click", function (e){
         e.preventDefault();
@@ -16,14 +25,38 @@ function next(){
         alert("oops! you missed something");
     }
     else{
-    location.href = 'https://www.google.co.il/';
+        UserObj.city = city;
+        UserObj.street = street;
+        UserObj.streetNumber = streetNumber;
+        let objString = JSON.stringify(UserObj); //now its string
+        localStorage.setItem('User', objString);
+    // location.href = 'phase3.html';
     }
 }
 
 
-
 function back(){
-    location.href = 'https://www.google.co.il/';
+    location.href = 'index.html';
 }
+
+
+function setUp() {
+
+    const city = document.querySelector("#city").value;
+    const street = document.querySelector("#street").value;
+    const streetNumber = document.querySelector("#streetNumber").value;
+
+    if (UserObj.city != null || UserObj.city != undefined) {
+        city.value = UserObj.city;
+    }
+    if (UserObj.street != null || UserObj.street != undefined) {
+        street.value = UserObj.street;
+    }
+    if (UserObj.streetNumber != null || UserObj.streetNumber != undefined) {
+        streetNumber.value = UserObj.streetNumber;
+    }
+}
+setUp();
+
 
 
